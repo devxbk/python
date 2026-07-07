c8ff03e3e0888491556e45ea143a78b80515aae5
devxbk <bobbybk03@gmail.com>
2026-07-07 18:10:24 +0530

    calculator program

--- calculator.py
+++ calculator.py
@@ -1,1 +1,23 @@
-print("Hello, Python!")
+def calculator(num1, num2, operator):
+    match operator:
+        case "+":
+            result = num1 + num2
+        case "-":
+            result = num1 - num2
+        case "*":
+            result = num1 * num2
+        case "/":
+            result = num1 / num2
+        case _:
+            print("Invalid Operator!")
+            return
+
+    print(f"Result: {result}")
+
+
+calculator(num1, num2, operator)
+
+while True:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    operator = input("Enter operator (+, -, *, /): ")
