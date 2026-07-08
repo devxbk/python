b6ef447e23ec2bedca88b74e9bcd5e0e22cc1dbf
devxbk <bobbybk03@gmail.com>
2026-07-08 17:28:03 +0530

    loop added

--- calculator.py
+++ calculator.py
@@ -7,6 +7,9 @@ def calculator(num1, num2, operator):
         case "*":
             result = num1 * num2
         case "/":
+            if num2 == 0:
+                print("Cannot divided by zero!")
+                return
             result = num1 / num2
         case _:
             print("Invalid Operator!")
@@ -15,9 +18,15 @@ def calculator(num1, num2, operator):
     print(f"Result: {result}")
 
 
-calculator(num1, num2, operator)
-
 while True:
     num1 = int(input("Enter first number: "))
     num2 = int(input("Enter second number: "))
     operator = input("Enter operator (+, -, *, /): ")
+
+    calculator(num1, num2, operator)
+
+    choice = input("Do you want to continue? (y/n): ").lower()
+
+    if choice != "y":
+        print("Goodbye!")
+        break
