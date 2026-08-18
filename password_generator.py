c7b7a2978790e277f469adc80ca95da486f8c40a
devxbk <bobbydev08@gmail.com>
2026-08-18 17:49:49 +0530

    password generator

--- password_generator.py
+++ password_generator.py
@@ -3,13 +3,51 @@ import random
 
 def password_genrator():
     try:
-        length = int(input("Enter the  length of the password (max 15): "))
+        length = int(input("Enter password length (8-15): "))
+        if length < 8 or length > 15:
+            print("Length must be between 8 and 15.")
+            return
     except ValueError:
-        print("Enter a valid length!")
+        print("Please enter a number.")
+        return
 
-    choice = input(" Inclusion of special characters (y/n)? : ").lower()
-
-    special_chars = [
+    all_chars = [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
         "!",
         "@",
         "#",
@@ -44,23 +82,22 @@ def password_genrator():
         "~",
     ]
 
+    choice = input(" Inclusion of special characters (y/n)? : ").lower()
+
     password = []
 
-    num_of_char = length
+    for i in range(length):
 
-    for i in range(num_of_char):
-        loopcount = random.randint(0, 9)
-        for j in range(loopcount):
-            rand_num = random.randint(0, 9)
-            password = password.append(rand_num)
+        if choice == "n":
+            chars = len(all_chars) - 32
+        else:
+            chars = len(all_chars)
 
-            num_of_char -= 1
+        rand_num = random.randint(0, (chars - 1))
+        rand_char = all_chars[rand_num]
+        password.append(rand_char)
 
-        rand_special = random.randint(0, 31)
-        special_char = special_chars[rand_special]
-        password = password.append(special_char)
-
-        num_of_char -= 1
+    print(f"Your password is: {''.join(map(str, password))}")
 
 
 password_genrator()
